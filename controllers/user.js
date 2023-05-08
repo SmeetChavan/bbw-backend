708500a7cc0341229397bd3e7bdbd0064a693b2e
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-05-08 18:59:39 +0530

    get -> post

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,8 @@ import Order from "../models/Order.js";
 
 export const myProfile = (req , res , next) => {
 
+    console.log(req.body);
+
     res.status(200).json({
         success : true,
         user : (req.user) ? req.user : "not logged in",
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,7 @@ router.get("/login" , passport.authenticate("google" , {
     successRedirect: process.env.FRONTEND_URL,
 }))
 
-router.get("/me" , isAuthenticated , myProfile)
+router.post("/me" , isAuthenticated , myProfile)
 
 router.get("/logout" , logout);
 
