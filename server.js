749d16ffa070c40f33c943c2761e73ad0e6807e8
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-05-08 11:54:29 +0530

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ export const instance = new Razorpay({
 // Using Middlewares
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     next();
 });  
 
