52755d32b62f1e8fb1d0249cfd2dbbf97a37ac75
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-05-08 15:55:12 +0530

    cookiename

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ import ErrorHandler from "../utils/ErrorHandler.js";
 
 export const isAuthenticated = (req , res , next) => {
 
-    const {token} = req.cookies;
+    const token = req.cookies["cookiename"];
 
     if(!token){
         return next(new ErrorHandler("Not Logged In" , 469));
