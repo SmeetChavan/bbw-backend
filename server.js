faf2beafb4d2584214c0118b590769b0506eabb6
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-05-08 12:43:40 +0530

    removed un-necessary lines

--- server.js
+++ server.js
@@ -31,11 +31,6 @@ export const instance = new Razorpay({
 
 // Using Middlewares
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    next();
-});
-
 app.use(session({
     name: "CookieName",
     secret: process.env.SESSION_SECRET,
