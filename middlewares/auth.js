294aa70d89e51698410684d3f4449595f0a77a6e
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-05-08 15:36:40 +0530

    request cookie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,24 +2,18 @@ import ErrorHandler from "../utils/ErrorHandler.js";
 
 export const isAuthenticated = (req , res , next) => {
 
-    const token = req.cookies['cookiename'];
+    const token = req.cookies.cookiename;
 
     if(!token){
-        console.log("No cookie found");
         return next(new ErrorHandler("Not Logged In" , 469));
     }
-
-    console.log(token);
-
     next();
 }
 
 export const isAdmin = (req , res , next) => {
 
     if(req.user.role !== 'admin'){
-        console.log("No admin");
         return next(new ErrorHandler("Only Admin Allowed" , 405));
     }
-
     next();
 }
