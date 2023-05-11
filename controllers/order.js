c04df20192c43d79d0271fe62bbaa97ddca5ebc6
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-05-11 10:46:20 +0530

    populating users' name

--- controllers/order.js
+++ controllers/order.js
@@ -91,7 +91,7 @@ export const paymentVerification = asyncError(async (req , res , next) => {
 });
 
 export const getMyOrders = asyncError(async (req , res , next) => {
-  
+
     const orders = await Order.find({
         user: req.user._id,
     }).populate('user' , 'name');
@@ -104,7 +104,7 @@ export const getMyOrders = asyncError(async (req , res , next) => {
 
 export const getAdminOrders = asyncError(async (req , res , next) => {
 
-    const orders = await Order.find({});
+    const orders = await Order.find({}).populate("user" , "name");
 
     res.status(200).json({
         success: true,
